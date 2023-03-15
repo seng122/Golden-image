a49de7e271b4d466a2e88d4b2cd51ed292676c00
seng122 <s.eng122@gmail.com>
2023-03-15 19:49:54 -0400

    pushing my first ec2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Golden-image
+So I am trying to set up a golden-image using packer
---
+++ main.tf
@@ -0,0 +1,31 @@
+## this is pulling the latest aws_ami
+data "aws_ami" "example" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  owners = ["amazon"]
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+## creating the ec2 instance pulling the latest aws_ami
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t2.micro"
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
