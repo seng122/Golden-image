fcdf8a6a5a3a38d7aa7b50f9f151df463b1cdb3c
seng122 <s.eng122@gmail.com>
2023-03-15 22:41:04 -0400

    pushing a packer build

--- main.tf
+++ main.tf
@@ -13,30 +13,14 @@ provider "aws" {
 }
 
 ## this is pulling the latest aws_ami
-data "aws_ami" "latest_amazon_linux" {
+data "aws_ami" "latest_packer_build" {
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-    filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
+  owners           = ["self"]
 }
 
 ## creating the ec2 instance pulling the latest aws_ami
 resource "aws_instance" "testing" {
-  ami           = data.aws_ami.latest_amazon_linux.id
+  ami           = data.aws_ami.latest_packer_build.id
   instance_type = "t2.micro"
   lifecycle {
     ignore_changes = ["ami"]
