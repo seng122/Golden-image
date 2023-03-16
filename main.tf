6fe7dc81a9fd3063ece054bb60fb25fc180f2d7b
seng122 <s.eng122@gmail.com>
2023-03-15 23:06:22 -0400

    adding in timestamp

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 ## this is pulling the latest aws_ami
 data "aws_ami" "latest_packer_build" {
   most_recent = true
-  owners           = ["self"]
+  owners           = [self]
 }
 
 ## creating the ec2 instance pulling the latest aws_ami
@@ -23,7 +23,7 @@ resource "aws_instance" "testing" {
   ami           = data.aws_ami.latest_packer_build.id
   instance_type = "t2.micro"
   lifecycle {
-    ignore_changes = ["ami"]
+    ignore_changes = [ami]
   }
 }
 
